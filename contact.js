576587535fc6d503985841daaa47f052869947f6
FalconTheBerd <136229185+FalconTheBerd@users.noreply.github.com>
2023-11-11 15:20:53 +1000

    Initial Commit

---
+++ contact.js
@@ -0,0 +1,40 @@
+function sendMessage() {
+    var name = document.getElementById("name").value;
+    var title = document.getElementById("title").value;
+    var description = document.getElementById("description").value;
+
+    // Validate input fields
+    if (name === '' || title === '' || description === '') {
+        alert('Please fill out all fields before sending the message.');
+        return;
+    }
+
+    // Create a JSON object with the data
+    var messageData = {
+        content: `**Name:** ${name}\n**Title:** ${title}\n**Description:** ${description}`
+    };
+
+    // Send the JSON data to the provided webhook URL
+    fetch('https://discord.com/api/webhooks/1171271218469548112/c8aspW3MZSOzpoOdSsIj0xqRU4loWUDhpuBol8BXzfoTfwfKrC8-DIBOi1x4jsSt_4eT', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(messageData),
+    })
+    .then(response => {
+        if (response.ok) {
+            console.log('Message sent successfully!');
+            alert('Message sent successfully!');
+            location.reload()
+        } else {
+            console.error('Error:', response.status);
+            alert('Failed to send message.');
+        }
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+        alert('Failed to send message.');
+    });
+    }
+
